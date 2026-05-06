659ad6a61bd1f7799c86e423be3a046d539ac43d
ByroVega10 <byro.vega@duocuc.cl>
2026-05-05 23:49:39 -0400

    Category Check

---
+++ src/categories/categories.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(
+    private readonly categoriesService: CategoriesService,
+  ) {}
+
+  @Post()
+  create(@Body() data: CreateCategoryDto) {
+    return this.categoriesService.create(data);
+  }
+
+  @Get()
+  findAll() {
+    return this.categoriesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.categoriesService.findOne(Number(id));
+  }
+}
---
+++ src/categories/categories.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: any) {
+    return this.prisma.category.create({
+      data,
+    });
+  }
+
+  findAll() {
+    return this.prisma.category.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.category.findUnique({
+      where: { id },
+    });
+  }
+}
